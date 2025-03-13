a86497b8ac2d4f15c52c898dc1da580a7b83a3a6
Omi <121783352+salunkheom@users.noreply.github.com>
2025-03-13 10:13:52 +0530

    Add files via upload
    boot

---
+++ script.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  // ===about us===
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
